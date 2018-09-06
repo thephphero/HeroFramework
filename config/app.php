aafd352d10479bdf29f8b7ed02f3261a5e4897cf
Celso Luiz de F. Fernandes <celsoluiz81@gmail.com>
2018-09-06 16:04:39 +0200

    Added application configuration files

---
+++ config/app.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The Hero Framework.
+ *
+ * (c) Celso Luiz de F. Fernandes  <celso@thephphero.com>
+ * Date: 29/05/18
+ * Time: 20:30
+ * Created by thePHPHero
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+   |--------------------------------------------------------------------------
+   | Autoloaded Bundles
+   |--------------------------------------------------------------------------
+   |
+   | The bundles listed here will be automatically loaded on the
+   | request to your application. Feel free to add your own bundles to
+   | this array to grant expanded functionality to the application.
+   |
+   */
+
+return [
+
+    'bundles'=>[
+        Bundles\FrameworkBundle\FrameworkBundle::class,
+        //Extensions\SecurityBundle\SecurityBundle::class,
+        //Extensions\TemplateBundle\TemplateBundle::class
+    ]
+];
