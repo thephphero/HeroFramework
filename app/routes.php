42f56a2c3e65df6e497ad1fb509b99abd93bea0a
Celso Luiz de F. Fernandes <celsoluiz81@gmail.com>
2018-09-07 16:27:03 +0200

    Added routes.php file

---
+++ app/routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Hero Framework.
+ *
+ * (c) Celso Luiz de F. Fernandes  <celso@thephphero.com>
+ * Date: 25/12/16
+ * Time: 13:43
+ * Created by thePHPHero
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
